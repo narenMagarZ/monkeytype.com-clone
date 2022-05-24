911db16d1c904af7e76725c322b6a1bc33875dbf
narenMagarZ <narenmagarz98@gmail.com>
2022-05-24 16:40:53 +0545

    changing the color of the letter according to the input key

--- src/App.js
+++ src/App.js
@@ -3,27 +3,37 @@ import TyperContainer from "./component/typercontainer/typercontainer";
 import { TEXT } from "./component/text";
 function App() {
   let cursorPos = 0
+  let prevPressedLetter = null
+  
   function ListenTypingEvent(){
-    document.body.addEventListener('keypress',(e)=>{
+    document.body.addEventListener('keydown',(e)=>{
       const textWrapper = document.getElementById('text-wrapper')
       const pressedKey = e.key
+      console.log(pressedKey)
       if(textWrapper){
         const validPressedKey = /^[A-z\W]{1}$/
         if(validPressedKey.test(pressedKey)){
-          console.log(pressedKey)
           const pressedElem = textWrapper.children[cursorPos]
           if(TEXT[cursorPos] === pressedKey){
-            pressedElem.style.color = "#00ff00"
+          if(pressedElem) pressedElem.style.color = "#0000ff"
           }
           else {
-            pressedElem.style.color = "#ff0000"
+           if(pressedElem) pressedElem.style.color = "#ff0000"
           }
           cursorPos ++
           console.log(cursorPos,'cursor position')
         }
+        else if(pressedKey === 'Backspace'){
+          cursorPos -- 
+          if(cursorPos < -1) cursorPos = 0
+          prevPressedLetter = textWrapper.children[cursorPos]
+          if(prevPressedLetter) prevPressedLetter.style.color = "#000000"
+
+        }
       }
     })
   }
+  
   ListenTypingEvent()
   return (
     <div className="app">
